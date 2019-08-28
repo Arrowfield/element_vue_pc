5c9109bb24d9385d2d5ea54bebb6c69e618137b9
Arrowfield <768449566@qq.com>
2019-08-28 09:16:53 +0800

    update

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,40 +1,6 @@
 <template>
   <div class="login">
-    <div class="login-form">
-      <div class="login-header">
-        <img src="../../assets/images/logo.svg" width="100" height="100" alt="">
-        <p>{{ $Config.siteName }}</p>
-      </div>
-      <el-input
-          placeholder="请输入用户名"
-          suffix-icon="fa fa-user"
-          v-model="userName"
-          style="margin-bottom: 18px"
-      >
-      </el-input>
-
-      <el-input
-          placeholder="请输入密码"
-          suffix-icon="fa fa-keyboard-o"
-          v-model="password"
-          type="password"
-          style="margin-bottom: 18px"
-          @keyup.native.enter="login"
-      >
-      </el-input>
-
-      <el-button
-          type="primary" :loading="loginLoading"
-          style="width: 100%;margin-bottom: 18px"
-          @click.native="login"
-      >登录
-      </el-button>
-      <div>
-        <el-checkbox v-model="Remember"> 记住密码</el-checkbox>
-        <a href="javascript:;" style="float: right;color: #3C8DBC;font-size: 14px">注册</a>
-      </div>
-
-    </div>
+    
   </div>
 </template>
 
