e6c642e9e0fcc7067d7974928944616f08d4443a
Arrowfield <768449566@qq.com>
2019-09-21 19:01:29 +0800

    update

--- src/components/my-canvas/src/my-canvas.vue
+++ src/components/my-canvas/src/my-canvas.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <canvas id="canvas" width="600" height="500" style="border:1px solid #ddd">
+        <canvas id="canvas" width="800" height="510" style="border:1px solid #ddd">
             你的浏览器不支持canvas，请升级浏览器
         </canvas>
         <div>
@@ -25,17 +25,20 @@
                     {value: "0.02", title: "政治课", color: "yellow"}
                 ],
                 imageUrl: require('@/assets/images/boys.png'),
-                dirIndex:0
+                dirIndex:0,
+                axis:{
+                    yAxis:[0,10,20,30,40,50,60],
+                    xAxis:[2017-10-24,2017-10-25,2017-10-26,2017-10-27]
+                }
             }
         },
         methods: {
             productPop() {
                 let canvas = document.getElementById('canvas')
                 let ctx = canvas.getContext('2d')
-                let image = new Image()
-                image.src = this.imageUrl
-                image.onload = () => {
-
+                //let image = new Image()
+                //image.src = this.imageUrl
+                /*image.onload = () => {
                     //ctx.drawImage(image,40,65,40,65,300,300,40,65)
                     let frameIndex = 0
                     setInterval(() => {
@@ -54,7 +57,20 @@
                         frameIndex++
                         frameIndex %= 4
                     }, 1000 / 10)
-                }
+                }*/
+                var 
+                baseX = 160,
+                baseY = 50,
+                pointWidth = 8,
+                originX = 40,
+                originY = 470;
+                ctx.lineWidth = 2
+                ctx.moveTo(originX,originY)
+                ctx.lineTo(originX,originY - this.axis.yAxis.length * baseY)
+                ctx.moveTo(originX,originY)
+                ctx.lineTo(originX + this.axis.xAxis.length * baseX,originY)
+                ctx.strokeStyle = "black"
+                ctx.stroke()
 
             }
         },
@@ -65,5 +81,7 @@
 </script>
 
 <style scoped>
-
+    #canvas{
+        background: #F3F3F3;
+    }
 </style>
