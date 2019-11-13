8e06bdec8ad02cc203fc84340997d3a890eaa502
Arrowfield <768449566@qq.com>
2019-11-13 23:16:43 +0800

    update

--- src/libs/DouglasPeucker.js
+++ src/libs/DouglasPeucker.js
@@ -7,6 +7,7 @@ export default class  DouglasPeucker {
     getMax(){
 
     }
+    //压缩点集合
     compress(from,to){
         let switchValue = false
         return this.points
@@ -14,5 +15,5 @@ export default class  DouglasPeucker {
 }
 
 /*
-* 算法：输入，输出，可行性，有穷性，确切性
+* 算法：输入，输出，可行性，有穷性，确切性，下方的时间显示（点、线一一对应）
 * */
