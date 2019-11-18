0ab04f68f54f96480ff93e96eaee8c1bf11bb05d
Arrowfield <768449566@qq.com>
2019-11-18 22:29:06 +0800

    update

--- src/components/data-svg/DataSvgYAxis.vue
+++ src/components/data-svg/DataSvgYAxis.vue
@@ -1,10 +1,13 @@
 <template>
     <svg xmlns="http://www.w3.org/2000/svg" x="0" :width="width">
-
+        <line></line>
     </svg>
 </template>
 
 <script>
+    /*
+    * 暂时不考虑：label的显示
+    * */
     export default {
         name: "DataSvgYAxis",
         data:{},
--- src/store/modules/dashboard/index.js
+++ src/store/modules/dashboard/index.js
@@ -1,9 +1,11 @@
 
 const DATA_SVG_WIDTH = 1640
+const DATA_SVG_SELECT = 240 //框选区域的最大是高度
 
 const state = {
-    width:1600,     //定义svg图形的标准宽度
-    axisYWidth:70   //定义svg图表中纵坐标的标准宽度
+    width:1600,      //定义svg图形的标准宽度
+    axisYWidth:70,   //定义svg图表中纵坐标的标准宽度
+    axisYHeight:240  //定义框选区域的最大是高度
 }
 
 const getters = {
